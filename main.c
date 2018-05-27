0f843762134b701a97c7dbd1c96dd76a68927407
Giuseppe Luzzi <peppe.luzzi@gmail.com>
2018-05-27 13:49:27 +0200

    Converted states in struct, loading tm structure fully working. todo: loading input

--- main.c
+++ main.c
@@ -2,8 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define str(x) #x
+#define xstr(x) str(x)
+
 typedef enum {MOVE_RIGHT, MOVE_STAY, MOVE_LEFT} moveType;
-typedef enum {true, false} bool;
+typedef enum {false, true} bool;
 
 typedef struct _transition {
 	int startState;
@@ -17,91 +20,134 @@ typedef struct _transition {
 typedef struct _state {
 	bool final;
 	transition *transitions;
-}
+} state;
 
 int main(int argc, char const *argv[])
 {
-	int step;
 	int statesSize;
 	int lastStatesSize;
+	int maxSteps;
 
-	int index;
-	char ch;
-	char move;
-	char line[11];
+	int parsing_step;
+	int parsing_index;
+	int parsing_state;
+	char parsing_ch;
+	char parsing_move;
+	char parsing_line[11];
 	
 	int i;
 	
 	statesSize = 10;
 	lastStatesSize= 10;
-	transition** states = (transition**) malloc(statesSize * sizeof(transition*));
+	state** states = (state**) malloc(statesSize * sizeof(state*));
 	for (i = 0; i < statesSize; i++) 
 		states[i] = NULL;
 
-	step = 0;
-	index = 0;
+	parsing_step = 0;
+	parsing_index = 0;
 	for (i = 0; i < 11; i++) 
-		line[i] = (char) 0;
+		parsing_line[i] = (char) 0;
 
-	while ((ch = getc(stdin)) != EOF) {
-		if (ch != '\n') {
-			line[index] = ch;
-			index++;
+	while ((parsing_ch = getc(stdin)) != EOF) {
+		if (parsing_ch != '\n') {
+			parsing_line[parsing_index] = parsing_ch;
+			parsing_index++;
 		} else {
-			if ((step == 0) && (strcmp(line, "tr") == 0)) {
-				step += 1;
-			} else if (step == 1) {
+			if ((parsing_step == 0) && (strcmp(parsing_line, "tr") == 0)) {
+				parsing_step = 1;
+			} else if (parsing_step == 1) {
 				transition* node = (transition*) malloc(sizeof(transition));
  				node->next = NULL;
-				if (sscanf(line, "%d%*c%c%*c%c%*c%c%*c%d", &(node->startState),
+				if (sscanf(parsing_line, "%d%*c%c%*c%c%*c%c%*c%d", &(node->startState),
 												   &(node->inChar),
 												   &(node->outChar),
-												   &move,
+												   &parsing_move,
 												   &(node->endState)) == 5) {
-					if (move == 'R')
+					if (parsing_move == 'R')
 						node->move = MOVE_RIGHT;
-					else if (move == 'S')
+					else if (parsing_move == 'S')
 						node->move = MOVE_STAY;
-					else if (move == 'L')
+					else if (parsing_move == 'L')
 						node->move = MOVE_LEFT;
 
-					printf(">> %d %c %c %d %d\n", node->startState, node->inChar, node->outChar, node->move, node->endState);
+					/*printf(">> %d %c %c %d %d\n", node->startState, node->inChar, node->outChar, node->move, node->endState);*/
 					
-					// Ingrandisco l'array quanto necessario
+					/* Ingrandisco l'array quanto necessario */
 					while ((node->startState > (statesSize - 1)) || (node->endState > (statesSize - 1))) {
 						statesSize *= 2;
 					}
-					
 					if (lastStatesSize != statesSize) {
-						states = (transition**) realloc(states, statesSize * sizeof(transition*));
+						states = (state**) realloc(states, statesSize * sizeof(state*));
 						for (i = lastStatesSize; i < statesSize; i++)
 							states[i] = NULL;
 
 						lastStatesSize = statesSize;
 					}
 
-					// Aggiungo lo stato alla array linked-list
-					if (states[node->startState] != NULL) {
-						node->next = states[node->startState];
-						states[node->startState] = node;
-					} else
-						states[node->startState] = node;
+					/* Aggiungo lo stato all'array linked-list*/
+					if (states[node->startState] == NULL) {
+						state* state_node = (state*) malloc(sizeof(state));
+						state_node->final = false;
+						state_node->transitions = NULL;
+						states[node->startState] = state_node;
+					}
+					if ((states[node->startState])->transitions != NULL) {
+						node->next = (states[node->startState])->transitions;
+						(states[node->startState])->transitions = node;
+					} else {
+						(states[node->startState])->transitions = node;
+					}
 
 
-				} else if (strcmp(line, "acc") == 0) {
-					step = 2;
+				} else if (strcmp(parsing_line, "acc") == 0) {
+					free(node);
+					parsing_step = 2;
+				}
+			} else if (parsing_step == 2) {
+				if (sscanf(parsing_line, "%d", &parsing_state) == 1) {
+					if (states[parsing_state] == NULL) {
+						state* state_node = (state*) malloc(sizeof(state));
+						state_node->final = true;
+						state_node->transitions = NULL;
+						states[parsing_state] = state_node;
+					} else 
+						states[parsing_state]->final = true;
+					/*printf(">>> #%d diventa finale\n", parsing_state);*/
+				} else if (strcmp(parsing_line, "max") == 0) {
+					parsing_step = 3;
+				}
+			} else if (parsing_step == 3) {
+				if (sscanf(parsing_line, "%d", &maxSteps) == 1) {
+					/*printf(">>> Max steps: %d\n", maxSteps);*/
+				} else if (strcmp(parsing_line, "run") == 0) {
+					parsing_step = 4;
 					break;
 				}
 			}
-			index = 0;
+			parsing_index = 0;
 			for (i = 0; i < 11; i++) 
-				line[i] = (char) 0;
+				parsing_line[i] = (char) 0;
 		}
 	}
 
+	printf(">>> Riepilogo\n");
+	printf("- Max steps: %d\n", maxSteps);
 	for (i = 0; i < statesSize; i++) {
-		if (states[i] != NULL)
-			printf("%d -> %d\n", states[i]->startState, states[i]->endState);
+		if (states[i] != NULL) {
+			printf("- Stato #%d (%d)\n", i, states[i]->final);
+			if (states[i]->transitions != NULL) {
+				transition *transitionCursor = states[i]->transitions;
+				while (transitionCursor != NULL) {
+					printf("\t %d -> %d [%c|%c|%d]\n",
+							transitionCursor->startState,
+							transitionCursor->endState,
+							transitionCursor->inChar,
+							transitionCursor->outChar,
+							transitionCursor->move);
+					transitionCursor = transitionCursor->next;
+				}
+			}
+		}
 	}
 
 	printf("oko\n");
